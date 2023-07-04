26da01417c568c849a02a50d204ec0d1b9e10dde
Brayan Soto Téllez <61593589+brayansoto99@users.noreply.github.com>
2023-07-04 09:04:28 -0600

    Add files via upload

---
+++ language/en.php
@@ -0,0 +1,55 @@
+<?php
+$titulo="Kangy Group";
+$titulo0="Language";
+$titulo1_barra="Home";
+$titulo2_barra="Services";
+$titulo3_barra="Contact";
+$banner1="Welcome to Kangy Group";
+$banner2="Union is strength and in Kangy Group we make a difference";
+$banner3="Union is strength and in Kangy Group we make a difference";
+$titulo1="Kangy Group";
+$contenido="Kangy Group consolidates the operation of 12 companies in the center of the country and integrates these different businesses facilitating their logistics among themselves and the distribution of the different products within the Mexican Republic.";
+$contenido2="At Kangy Group we are committed to satisfy the needs and expectations of our customers, providing efficient products and services at competitive costs, continuously improving our processes.";
+$titulo2="About Kangy Group";
+$titulo2_1="Mission";
+$contenido2_1="Through continuous improvement and with the participation of our staff, shareholders and suppliers, in Kangy Group we provide products and services, contributing to the care of our environment.";
+$titulo2_2="Vision";
+$contenido2_2="To be an internationally recognized group, promoting innovation and continuous improvement to provide quality products and services. Committed to the integral growth of all those involved.";
+$titulo2_3="Values";
+$contenido2_3_1="Commitment.";
+$contenido2_3_2="Respect.";
+$contenido2_3_3="Ethics.";
+$titulo_final="Please keep in contact through our social networks.";
+
+
+
+$cont1="Kangy Group specializes in different areas of the industry, in which our different affiliated companies provide the best services.";
+$cont2="Our companies produce, process, distribute and market a wide range of items that provide solutions to different markets; always guided by the continuous improvement. In Kangy Group we seek to cover the different requirements of our clients and also that of our collaborators, allowing us to continuously improve their quality of life.";
+$categoria1="Metal - Mechanic";
+$empresa1_1="OYM is a company specialized in the manufacture of ball valves and male valves in Carbon Steel and Stainless Steel under the strictest national and international standards.";
+$empresa1_2="It�s a company specialized in steel casting.";
+$empresa1_3="It�s a company specialized in the manufacture of chocolate molds and industrial tools.";
+$categoria2="Plastics";
+$empresa2_1="Injection of plastics for the home.";
+$categoria3="Recycler";
+$empresa3_1="Recycled products of PP and PET.";
+$categoria4="Marketers";
+$empresa4_1="Marketing and distribution of products for the home.";
+$empresa4_2="Agricultural products and services.";
+$empresa4_3="Marketing and distribution of chemical-pharmaceutical products.";
+$categoria5="Education";
+$empresa5_1="Provides preschool, elementary and high school education.";
+$bannerrojo="We would like to help and contact you";
+$botonbanner="Contact Us";
+
+
+$bannercont1="Leave us a message, your opinion is important to us";
+$tituloform="Leave us a message.";
+$campo1form="Full name";
+$campo2form="Email";
+$campo3form="Subject";
+$campo4form="Message";
+$boton11="SEND";
+$tituloder="Please keep in contact";
+$contenidoder="We will gladly answer your questions or suggestions in any of our multiple contact options.";
+
---
+++ language/es.php
@@ -0,0 +1,55 @@
+<?php
+$titulo0="Idioma";
+$titulo1-barra="Inicio";
+$titulo2-barra="Servicios";
+$titulo3-barra="Contacto";
+$banner1="Bienvenido a Grupo Kangy";
+$banner2="La uni?n hace la fuerza y en Grupo Kangy hacemos la diferencia";
+$banner3="La uni?n hace la fuerza y en Grupo Kangy hacemos la diferencia";
+$titulo1="Grupo Kangy";
+$contenido="Grupo Kangy consolida la operaci?n de 12 empresas en el centro del pa?s e integra estos diferentes negocios facilitando su log?stica entre si y la distribuci?n de los diferentes productos dentro de la Rep?blica Mexicana.";
+$contenido2="En Grupo Kangy estamos comprometidos a satisfacer las necesidades y expectativas de nuestros clientes, brindando productos y servicios eficientes a costos competitivos, mejorando continuamente nuestros procesos.";
+$titulo2="Sobre Grupo Kangy";
+$titulo2-1="Misi?n";
+$contenido2-1="A trav?s de la mejora continua y con la participaci?n de nuestro personal, accionistas y proveedores, en Grupo Kangy brindamos productos y servicios, contribuyendo al cuidado de nuestro medio ambiente.";
+$titulo2-2="Visi?n";
+$contenido2-2="Ser un grupo reconocido a nivel internacional, promoviendo la innovaci?n y mejora continua para proporcionar productos y servicios de calidad. Comprometidos con el crecimiento integral de todos los involucrados.";
+$titulo2-3="Valores";
+$contenido2-3-1="Compromiso.";
+$contenido2-3-2="Respeto.";
+$contenido2-3-3="Ética.";
+$titulo-final="Por favor sigue en contacto a trav?s de nuestras redes sociales.";
+
+SERVICIOS
+$cont1="Grupo Kangy se especializa en distintas ?reas de la industria, en las cuales nuestras distintas empresas afiliadas brindan los mejores servicios";
+$cont2="Nuestras empresas producen, procesan, distribuyen y comercializan una amplia gama de art?culos que brindan soluciones a diferentes mercados; guiados siempre por la mejora contin?a. En grupo Kangy buscamos cubrir los diferentes requerimientos de nuestros clientes y tambi?n la de nuestros colaboradores perminti?ndonos mejorar continuamente su calidad de vida.";
+$categoria1="Metal - Mec?nico";
+$empresa1-1="OYM es una empresa especializada en la fabricaci?n de v?lvulas de bola y v?lvulas macho en Acero al carb?n y Acero Inoxidable bajo las m?s estrictas normas nacionales e internacionales.";
+$empresa1-2="Es una empresa especializada en la fundici?n de acero.";
+$empresa1-3="Es una empresa especializada en la fabricaci?n de moldes de chocolate y herramientas industriales.";
+$categoria2="Pl?sticos";
+$empresa2-1="Inyecci?n de plasticos para el hogar.";
+$categoria3="Recicladoras";
+$empresa3-1="Productos reciclados de PP y PET.";
+$categoria4="Comercializadoras";
+$empresa4-1="Comercializaci?n y distribuci?n de productos para el hogar.";
+$empresa4-2="Prodcutos y servicios agricolas.";
+$empresa4-3="Comercialización y distribución de productos quimico-farmaceuticos.";
+$categoria5="Educaci?n";
+$empresa5-1="Brinda educaci?n en preescolar, primaria y secundaria.";
+$bannerrojo="Nos gustaría ayudarlo y contactarnos con usted";
+$botonbanner="CONTÁCTENOS";
+
+CONTACTO
+$bannercont1="D?jenos un mensaje, su opini?n es importante para nosotros";
+$tituloform="Déjenos un mensaje.";
+$campo1form="Nombre completo";
+$campo2form="Correo";
+$campo3form="Asunto";
+$campo4form="Mensaje";
+$boton11="ENVIAR";
+$tituloder="Mant?ngase en contacto"
+$contenidoder="Con gusto atenderemos sus dudas o sugerencias en cualquiera de nuestras m?ltiples opciones de contacto.";
+
+
+?>
